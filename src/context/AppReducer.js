adde2f983d49a5c5dac69b0fc8b9c086016dfc66
simbankenya <pharesmuruthi@gmail.com>
2021-06-04 13:44:45 +0300

    initial commit

---
+++ src/context/AppReducer.js
@@ -0,0 +1,11 @@
+export default (state, action)=> {
+    switch (action.type) {
+        // case value:
+        // case value:
+        // case value:
+        // case value:
+    
+        default:
+            return state;
+    }
+}
